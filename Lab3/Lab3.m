39fe700af7c439cc9f4a478fbc62e4f45798f85b
Christian <christianhabenson@gmail.com>
2020-01-14 15:32:45 +0100

    done lab 3

--- Lab3/Lab3.m
+++ Lab3/Lab3.m
@@ -18,20 +18,30 @@ y = [ 3.749 4.689 6.273 5.897 6.381 7.003 ];
 plot(x,y,'+')
 hold on
 p = polyfit(x,y,5);
-x = linspace(-10,10);
+x = linspace(-10,10,1000);
 plot(x, polyval(p, x));
+axis([-3 7 0 10])
 
 %% D3
 clc
 clear
 
 x = [ 0.10 0.20 0.30 0.40 0.50 0.60 0.70 0.80 0.90 1.00 1.10 1.20 1.30 1.40 1.50 ];
-y = [ 2.48 2.75 2.68 3.12 3.53 3.66 3.50 3.34 3.77 3.80 3.75 4.59 4.51 4.37 4.30 ];
+y = [ 1.77 1.98 2.19 2.23 2.38 2.77 2.81 2.99 3.12 3.17 3.21 3.71 3.48 3.36 3.90 ]
+
+ysquared = y.^2; % Square to approximate with kx+m
+params = polyfit(x,ysquared,1) % kx+m approximation
+
+params(2) = params(2)/params(1); % Find values for a and b
+params(1) = sqrt(params(1));
 
 plot(x,y,'+')
 hold on
 b = table(x,y);
-E = [ones(size(x)), ]
+xparam = linspace(0,3);
+plot(xparam, params(1).*sqrt(xparam + params(2)));
+
+
 
 %% D4
 clc
@@ -76,7 +86,8 @@ rho = 1.29;
 
 f = @(v) 4*M/r - (C*rho*A*v.^2)/2;
 
-a = @(t, v) f(v)./M;
+mass = 2107.98;
+a = @(t, v) f(v)./mass;
 tspan = [0 3];
 y0 = 0;
 [t, y] = ode23(@(t,y) a(t,y), tspan, y0);
